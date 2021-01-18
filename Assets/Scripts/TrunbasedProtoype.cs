1cde03c564bdc33efff636c2f35daac75b861e0d
Robin Alderin <94robald@gmail.com>
2021-01-18 15:03:18 +0100

    First try
    added 1 script and 2 buttons

---
+++ Assets/Scripts/TrunbasedProtoype.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunbasedProtoype : MonoBehaviour
+{
+    public List<GameObject> players;
+    void Start()
+    {
+        new List<GameObject>(players);
+    }
+
+    void Update()
+    {
+        
+    }
+}
