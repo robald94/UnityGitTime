ed6c9f398faedd08b6100779ce69ffa642770521
Robin Alderin <94robald@gmail.com>
2021-01-18 16:24:23 +0100

    Diceroll
    Works alright

--- Assets/Scripts/TrunbasedProtoype.cs
+++ Assets/Scripts/TrunbasedProtoype.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrunbasedProtoype : MonoBehaviour
 {
+    public int diceRoll;
+    public int maxDiceRoll = 10;
+    public int turnorder;
+
+    public Text diceRollText;
+
     public List<GameObject> players;
     void Start()
     {
@@ -12,6 +19,40 @@ public class TrunbasedProtoype : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            DiceRoller();
+        }
+    }
+
+
+    public void DiceRoller()
+    {
+        diceRoll = Random.Range(1, maxDiceRoll);
+        diceRollText.text = diceRoll.ToString();
+        Debug.Log(diceRoll);
+        PlayerTurnOrder();
+    }
+
+    public void PlayerTurnOrder()
+    {
+        switch (turnorder)
+        {
+            case 1:
+                if(diceRoll == Random.Range(1, 5))
+                {
+                    print("Playerone starts");
+                }
+                break;
+            case 2:
+                if (diceRoll == Random.Range(6, 10))
+                {
+                    print("Playertwo starts");
+                }
+                break;
+            default:
+                print("Playerone Starts");
+                break;
+        }
     }
 }
